a968b19b0aa76b2f3cc52ba496deca2951f90dc6
Szilveszter Farkas <szilveszter.farkas@rackspace.com>
2022-10-14 11:52:17 +0200

    Fix invoke_url output reference

--- modules/apigateway-v1/outputs.tf
+++ modules/apigateway-v1/outputs.tf
@@ -10,5 +10,5 @@ output "invoke_url" {
   # If we have a custom domain set up, we should point the endpoint to that.
   # Otherwise take the deployment's invoke URL, and strip the intermediary
   # stage suffix from the URL.
-  value = var.enable_custom_domain ? format("https://%s", var.custom_domain) : aws_api_gateway_deployment.stage.invoke_url
+  value = var.enable_custom_domain ? format("https://%s", var.custom_domain) : aws_api_gateway_deployment.deployment.invoke_url
 }
