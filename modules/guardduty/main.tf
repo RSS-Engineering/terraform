054b9da71c2a36f5d85e582d5e310d7322946d63
Scott Mayberry <scott.mayberry@rackspace.com>
2024-02-15 08:52:03 -0500

    Remove deprecated datasources

--- modules/guardduty/main.tf
+++ modules/guardduty/main.tf
@@ -1,23 +1,5 @@
 resource "aws_guardduty_detector" "detector" {
   enable = true
-
-  datasources {
-    s3_logs {
-      enable = true
-    }
-    kubernetes {
-      audit_logs {
-        enable = false
-      }
-    }
-    malware_protection {
-      scan_ec2_instance_with_findings {
-        ebs_volumes {
-          enable = false
-        }
-      }
-    }
-  }
 }
 
 resource "aws_guardduty_detector_feature" "lambda_network_logs" {
