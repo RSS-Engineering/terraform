7ecb579ef9acdb3f7a14880d7db2720c63ccbfaf
sai-praveen-os <saipraveen.os@rackspace.com>
2026-02-11 14:43:56 +0530

    fix(PLATEXP-11106): use @ts-ignore for janus-core ES module imports with nodenext resolution

--- modules/connectivity_check/lambda/index.ts
+++ modules/connectivity_check/lambda/index.ts
@@ -1,6 +1,8 @@
 import { Socket } from 'net';
 import { lookup } from 'dns/promises';
+// @ts-ignore - janus-core doesn't have proper ES module type declarations
 import stats from '@racker/janus-core/lib/stats/index.js';
+// @ts-ignore - janus-core doesn't have proper ES module type declarations
 import log from '@racker/janus-core/lib/log/index.js';
 
 interface TestTarget {
